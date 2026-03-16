e5cfe0b7c8b9bc10c0f5129a409b4a5d8956577e
Nechami kolman <email@example.com>
2026-03-16 22:13:43 +0200

    middelof level 9

---
+++ project .net/DalTest/DalXml/Config.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DalXml
+{
+    internal static class Config
+    {
+        private const string nameconfig = "data-config";
+        public static int ProductNum
+        {
+            get
+            {
+                ProductNum++;
+                return 0;
+            }
+        }
+
+    }
+}
